6419547e3f0059cb0cc213c4aedee182a327ecff
unknown <jonahwelt94@gmail.com>
2026-02-01 17:11:05 -0500

    chore: update'

--- installDependencies.sh
+++
@@ -1,9 +0,0 @@
-py -m pip install --upgrade pip
-py -m pip install notebook
-py -m pip install numpy
-py -m pip install pandas
-py -m pip install matplotlib
-py -m pip install keras
-py -m pip install sklearn
-py -m pip install seaborn
-py -m pip install scikit-learn
