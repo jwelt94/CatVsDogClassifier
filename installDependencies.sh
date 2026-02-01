290b1c4b53393626507c9f14cd7c1bedef6e7648
unknown <jonahwelt94@gmail.com>
2026-02-01 15:00:10 -0500

    feat: update depdendency installer'

---
+++ installDependencies.sh
@@ -0,0 +1,9 @@
+pip install notebook
+pip install numpy
+pip install pandas
+pip install matplotlib
+pip install tensorflow
+pip install keras
+pip install sklearn
+pip install seaborn
+pip install scikit-learn
