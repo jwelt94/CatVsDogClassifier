4f26972ac218e03c053f733900df1f07a68152ec
unknown <jonahwelt94@gmail.com>
2026-02-01 15:09:01 -0500

    update depency installer

--- .ipynb_checkpoints/installDependencies-checkpoint.sh
+++ .ipynb_checkpoints/installDependencies-checkpoint.sh
@@ -1,9 +1,9 @@
-pip install notebook
-pip install numpy
-pip install pandas
-pip install matplotlib
-pip install tensorflow
-pip install keras
-pip install sklearn
-pip install seaborn
-pip install scikit-learn
+py -m pip install notebook
+# pip install numpy
+# pip install pandas
+# pip install matplotlib
+# pip install tensorflow
+# pip install keras
+# pip install sklearn
+# pip install seaborn
+# pip install scikit-learn
